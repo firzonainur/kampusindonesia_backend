17777ed4b759cd21e34a52a1fc78c3c00e6cbcec
Damario Aditama <damarioaditama007@gmail.com>
2020-10-08 16:29:50 +0700

    Mengerjakan dashboard.php dan artikel.php juga menambahkan file function.php

---
+++ artikel.php
@@ -0,0 +1,81 @@
+<?php 
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "kampus_indonesia");
+
+$connect = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
+$artikelDB =mysqli_query($connect, "SELECT * FROM artikel");
+
+if(!$artikelDB){
+	echo mysqli_error($connect);
+}
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kampus Indonesia - Artikel</title>
+	<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #00BFFF;
+  color: white;
+}
+</style>
+</head>
+<body>
+<h1>ARTIKEL</h1>
+<div class="topnav">
+  <a class="active" href="artikel.php">Artikel</a>
+  <a href="dashboard.php">Dashboard</a>
+  <a href="login.php">Logout</a>
+</div>
+<?php while ( $row = mysqli_fetch_assoc($artikelDB)) { ?>
+	<article>
+	<tr>
+		<td><img src="img/<?php echo $row["gambar_artikel"];?>" width="100"></td>
+	</tr>
+
+	<br>
+
+	<tr>
+		<td><h1><?php echo $row["judul_artikel"] ?></h1></td>
+	</tr>
+
+	<br>
+
+	<tr>
+		<td><?php echo $row["teks_artikel"] ?></td>
+	</tr>
+	<br>
+	</article>
+	
+<?php } ?>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,9 +1,61 @@
+<?php 
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "kampus_indonesia");
+
+$connect = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
+
+//cara menulis query
+$check =mysqli_query($connect, "SELECT * FROM users");
+
+if(!$check){
+	echo mysqli_error($connect);
+}
+
+
+?>
+
+<?php
+while ($account = mysqli_fetch_assoc($check)) {
+
+	if(isset($_POST["login"])){
+	if ($_POST["username"]== $account["username"] && $_POST["password"] == $account["password"]) {
+		header("Location: dashboard.php");
+		exit;
+	}else{
+		$error = true;
+	}
+}
+
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $judul ?></title>
+	<title>Kampus Indonesia-Login</title>
 </head>
 <body>
 <h1>Anda sudah masuk di form login</h1>
+<ul>
+<form action="" method="post">
+	<li>
+		<label for="username">Username:</label> <input type="text" name="username" id="username">
+	</li>
+	<li>
+		<label for="password">Password:</label><input type="Password" name="password" id="password">
+	</li>
+	<li>
+		<button type="submit" name="login">Login</button>
+	</li>
+	<li>
+		<?php if(isset($error)){ ?>
+			<p style="color: red">Username atau Password anda salah!</p>
+		<?php } ?>
+	</li>
+</form>
+</ul>
 </body>
 </html>
