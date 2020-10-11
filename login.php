40d54532bd52b389b90eda8f70ba695c82b9d4e7
70k0b0d0 <70k0b0d0@gmail.com>
2020-10-11 22:34:54 +0700

    update

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
 		<?php 
 			if(isset($_GET['pesan'])){
 			if($_GET['pesan'] == "gagal"){
-				echo "Login gagal! username dan password salah!";
+				echo "Login gagal! username atau password salah!";
 			}else if($_GET['pesan'] == "logout"){
 				echo "Anda telah berhasil logout";
 			}else if($_GET['pesan'] == "belum_login"){
