c36ae1a6322c09e726c1044790e9ce36c8a5344f
avianlukmans <58462392+avianlukmans@users.noreply.github.com>
2020-10-13 11:40:19 +0700

    Update and rename index.php to welcome.php

---
+++ welcome.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage - Library</title>
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row" align="center">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="background: #A3D7E0;">
+				<h4 style="font-family: cursive; color: #176687;">Selamat datang di Kampus Indonesia!</h4>
+				<br><br>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" align="left">
+						<ul style="color: #0E465D">
+							<li><a href="indexBuku.php">Artikel Hari Ini</a></li>
+							<li><a href="indexMahasiswa.php">Pengunjung Hari Ini</a></li>
+							<li><a href="indexUser.php">User</a></li>
+
+						</ul>
+					</div>
+				</div>			
+			</div>
+		</div>
+	</div>
+</body>
+</html>
