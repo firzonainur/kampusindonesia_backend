77a445ccb2e42cd568b64b05f114563be223aeec
avianlukmans <58462392+avianlukmans@users.noreply.github.com>
2020-10-13 11:23:21 +0700

    Update login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Site</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<form method="post" action="ceklogin.php">
+	<label>Login</label>
+	<hr>
+	<label>Username</label><br>
+	<input type="text" name="username"><br>
+	<label>Password</label><br>
+	<input type="password" name="password"><br>
+	<button type="submit">Login</button>
+</form>
+</body>
+</html>
